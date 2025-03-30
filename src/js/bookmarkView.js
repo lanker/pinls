5c9642bcaf80a7411e3c7fc0814c41037ebb9074
Fredrik Lanker <fredrik@lanker.se>
2025-03-30 21:40:38 +0200

    Added action buttons and some styling to BookmarkView

--- src/js/bookmarkView.js
+++ src/js/bookmarkView.js
@@ -36,16 +36,30 @@ class BookmarkView extends HTMLElement {
         eUrl.textContent = this._bookmark.url;
         eContainer.appendChild(eUrl);
 
-        const eDescription = document.createElement("div");
-        eDescription.textContent = this._bookmark.description;
-        eContainer.appendChild(eDescription);
+        if (this._bookmark.description) {
+            const eDescriptionTitle = document.createElement("h3");
+            eDescriptionTitle.textContent = "Description";
+            eContainer.appendChild(eDescriptionTitle);
+            const eDescription = document.createElement("div");
+            eDescription.textContent = this._bookmark.description;
+            eContainer.appendChild(eDescription);
+        }
 
-        const eNotes = document.createElement("div");
-        eNotes.textContent = this._bookmark.notes;
-        eContainer.appendChild(eNotes);
+        if (this._bookmark.notes) {
+            const eNotesTitle = document.createElement("h3");
+            eNotesTitle.textContent = "Notes";
+            eContainer.appendChild(eNotesTitle);
+            const eNotes = document.createElement("div");
+            eNotes.textContent = this._bookmark.notes;
+            eContainer.appendChild(eNotes);
+        }
 
         // tags (create component)
-        {
+        if (this._bookmark.tag_names.length > 0 || this._bookmark.unread) {
+            const eNotesTitle = document.createElement("h3");
+            eNotesTitle.textContent = "Tags";
+            eContainer.appendChild(eNotesTitle);
+
             const url = new URL(window.location.href);
             const eTags = document.createElement("div");
             eTags.classList.add("bookmark-tags");
@@ -64,12 +78,25 @@ class BookmarkView extends HTMLElement {
                 eTag.classList.add("bookmark-tag");
                 eTags.appendChild(eTag);
             }
+
+            if (this._bookmark.unread) {
+                const eTag = document.createElement("a");
+                url.searchParams.set("unread", "yes");
+                url.searchParams.delete("offset");
+                eTag.href = url.href;
+                eTag.textContent = "unread";
+                eTag.classList.add("bookmark-tag");
+                eTag.classList.add("bookmark-tag-unread");
+                eTags.appendChild(eTag);
+            }
+
             eContainer.appendChild(eTags);
         }
+
         // metadata
         {
             const eMetadata = document.createElement("div");
-            eMetadata.classList.add("bookmark-view-metadata-container");
+            eMetadata.classList.add("bookmark-metadata-container");
 
             const eDateAdded = document.createElement("span");
             eDateAdded.textContent = `Added: ${new Date(Date.parse(this._bookmark.date_added)).toLocaleDateString()}`;
@@ -84,12 +111,80 @@ class BookmarkView extends HTMLElement {
             eContainer.appendChild(eMetadata);
         }
 
+        // actions
+        {
+            const eActions = document.createElement("div");
+            eActions.classList.add("bookmark-actions");
+
+            const eEdit = document.createElement("a");
+            eEdit.href = `add.html?id=${this._bookmark.id}`;
+            eEdit.textContent = "Edit";
+            eActions.appendChild(eEdit);
+
+            const eDivider = document.createElement("span");
+            eDivider.textContent = " | ";
+            eActions.appendChild(eDivider);
+
+            const eDeleteContainer = document.createElement("span");
+            const eDelete = document.createElement("button");
+            eDelete.type = "button";
+            eDelete.textContent = "Delete";
+            eDelete.id = "delete-button";
+            eDelete.onclick = () => {
+                const eDivider1 = document.createElement("span");
+                eDivider1.textContent = "?: ";
+                eDeleteContainer.appendChild(eDivider1);
+
+                const eYes = document.createElement("button");
+                eYes.textContent = "yes";
+                eDeleteContainer.appendChild(eYes);
+                eYes.onclick = async () => {
+                    const server = localStorage.getItem("pinrs-server");
+                    const token = localStorage.getItem("pinrs-token");
+                    if (!this._bookmark || !server || !token) {
+                        return;
+                    }
+                    try {
+                        const url = new URL(`${server}/api/bookmarks/${this._bookmark.id}/`);
+                        await fetch(url, {
+                            method: "DELETE",
+                            headers: {
+                                "Content-type": "application/json",
+                                Authorization: `Token ${token}`,
+                            },
+                        });
+                        window.location.href = "index.html";
+                    } catch (err) {
+                        console.error(err);
+                        console.error("Failed to delete", this._bookmark.id);
+                        eDeleteContainer.replaceChildren(eDelete);
+                    }
+                };
+                const eDivider2 = document.createElement("span");
+                eDivider2.textContent = " | ";
+                eDeleteContainer.appendChild(eDivider2);
+
+                const eNo = document.createElement("button");
+                eNo.textContent = "no";
+                eNo.onclick = () => {
+                    eDeleteContainer.replaceChildren(eDelete);
+                };
+                eDeleteContainer.appendChild(eNo);
+            };
+            eDeleteContainer.appendChild(eDelete);
+            eActions.appendChild(eDeleteContainer);
+
+            eContainer.appendChild(eActions);
+        }
+
         const eClose = document.createElement("button");
+        eClose.classList.add("close-button");
         eClose.textContent = "Close";
         eClose.type = "button";
         eClose.onclick = () => {
             this.remove();
         };
+
         eContainer.appendChild(eClose);
 
         shadow.appendChild(eContainer);
